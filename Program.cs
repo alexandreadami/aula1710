4f6d89a85b281ce8286ab4b9cba5efb8421ca803
FIC <alexandre.adami@uol.com.br>
2017-10-17 19:12:53 -0200

    19:12

--- Program.cs
+++ Program.cs
@@ -6,7 +6,32 @@ namespace aula1710
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int salario = 2;
+
+            while (salario < 10)
+            {
+                salario *= 2;
+                Console.WriteLine("Seu salário é: " + salario);
+            }
+
+            string parar = "";
+
+            while (parar != "s")
+            {
+                Console.WriteLine("Desena sair do programa? s/n");
+                parar = Console.ReadLine();
+            }
+
+            //int i;
+            int tabuada = 3;
+            for (int i = 0; i <= 10; i++)
+            {
+                int resultado;
+                resultado = tabuada * i;
+                Console.WriteLine(tabuada + " x " + i + " = " + resultado);
+            }
+
+            
         }
     }
 }
